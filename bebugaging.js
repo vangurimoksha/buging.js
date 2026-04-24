b834ebc646878197bc4b97c51ab59e226a88a1aa
vangurimoksha <vanguri4638@gmail.com>
2026-04-24 15:00:45 +0530

    closed #1

--- bebugaging.js
+++ bebugaging.js
@@ -1,5 +1,5 @@
 function login(username, password) {
-    if (username == "admin" && (password == "1234")) {
+    if (username == "admin" && password == "1234") {
         return "Login Success";
     } else {
         return "Login Failed";
